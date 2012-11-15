fae31b30e0e99411a7cc626fa2eded2b8a604861
Nate Marks <nate.marks@pobox.com>
2012-11-15 09:26:40 -0500

    dfrg

--- python-silk-examples/topTalkersReport.py
+++ python-silk-examples/topTalkersReport.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+#  
 import sys, os, subprocess, smtplib, time
 
 def runBash(cmd):
@@ -15,6 +15,10 @@ enddate = runBash("date +%Y/%m/%d:%H")
 
 open('autosilk.data','w')
 open('autosilk.data','a').write("Top TalkersReport for Last 12 Hours.\n\n")
+
+
+for host in sensors:
+runBash("rwfilter --start-date=%s --end-date=%s --protocol=1,6,17 --sensor= --type=all --pass=stdout | rwstats --count=10 --fields=sip,$")
 
 
 
