eff89d8b4b4beac75f814cc323c96c946dbbaefd
Nate Marks <nate.marks@pobox.com>
2012-11-15 08:47:01 -0500

    adding some example python scripts.  This one is taken from Sanders Smith's  Real World Security Scripting presentation.

---
+++ python-silk-examples/topTalkersReport.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys, os, subprocess, smtplib, time
+
+def runBash(cmd):
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+
+    out = p.stdout.read().strip()
+    return out
+
+
+sensors = ["CHECORE1"]
+startdate = runBash("date -d '-12 hour'+%Y/%m/%d:%H")
+enddate = runBash("date +%Y/%m/%d:%H")
+
+open('autosilk.data','w')
+open('autosilk.data','a').write("Top TalkersReport for Last 12 Hours.\n\n")
+
+
+
